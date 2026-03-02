d5addd1e59e306a9cb49a90ec75ffe3e2ebc0953
ViKas Jaiswal0202 <vikasjaiswal0202@ibm.com>
2026-03-03 02:20:31 +0530

    commited at 2:20 lambda new

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
+import json
+
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "CI/CD Pipeline Working!"
+        "body": json.dumps("CI/CD Pipeline Working and this is night 2:20")
     }
